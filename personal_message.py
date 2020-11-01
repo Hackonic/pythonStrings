84c0a1be2e0bc16908a328d701e0ebaf1061537e
Hackonic <73733369+Hackonic@users.noreply.github.com>
2020-11-01 14:55:02 +0000

    HACKONIC
    THIS ONLY OWN TRAIL CODES WITH GENERATED BY HACKONIC

---
+++ personal_message.py
@@ -0,0 +1,43 @@
+#first string program 
+personal_message="hello cyborg , would you like to learn python !"
+print()
+print(personal_message)
+print()
+print("*"*50)
+
+#second string program
+msg1="hello eric,"
+msg2="would you like to learn python !"
+print(msg1+" "+msg2)
+print("*"*50)
+
+#third string program
+enter_name=input("enter you name :")
+msg="would you like to learn python"
+print('hello'+" "+enter_name+(",")+msg)
+print("*"*50)
+
+#fourth string program
+third_name="hello"
+third_second="eric"
+mesg="would you like to learn python"
+print("{} {},{}".format(third_name,third_second,mesg))
+print("*"*50)
+
+#five program
+print()
+five_hello="hello"
+five_name="eric,"
+five_msg="Would you like to learn python program "
+print("{0} {1} {2}".format(five_hello,five_name,five_msg))
+print("*"*50)
+
+#six program
+six_hello=input("enter hello message :")
+six_name=input("enter your name :")
+six_message=input("enter you message : ")
+print("*"*100)
+print("{} {}, {}".format(six_hello,six_name,six_message))
+print("*"*100)
+print("-"*100)
+
---
+++ personal_trialcode.py
@@ -0,0 +1,48 @@
+#first program
+print("*"*100)
+first_msg="hello eric"
+second_msg2="would you like to learn python !"
+print(first_msg+" "+second_msg2)
+print("*"*100)
+
+#second program
+hello="hello"
+name="eric,"
+msg="would you like to learn python"
+print(hello,name,msg)
+print("*"*100)
+
+#third program
+hello="hello"
+name="emy"
+msg="would you like to learn python"
+print(hello+" "+name+(",")+msg)
+print("*"*100)
+
+#fourth program
+hello=input("enter your hello wishes :")
+name=input("enter your name :")
+msg=input("enter your message:")
+print(hello,name,msg)
+print("*"*100)
+
+#five program
+hello=input("enter your hello Msg:")
+name=input("enter your name :")
+msg=input("enter your message : ")
+print("{} {} {} ".format(hello,name,msg))
+print("*"*100)
+
+#six program
+hello_msg="hello Good morning"
+name="Eric,"
+Msg="Would You like to Learn Python"
+print("{} {} {}".format(hello_msg,name,msg))
+print("*"*100)
+
+#seven program
+hello="hello"
+name="ROSI"
+Msg="would you like to learn python"
+print("{2} {0}, {1}".format(name,Msg,hello))
+print("*"*100)
